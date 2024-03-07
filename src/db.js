a241cce877747845db92562978b43d10d6b8d67d
Miguel <mipe127@gmail.com>
2024-03-06 21:17:51 -0500

    conexion base de datos en la nube con mongodb atlas

--- src/db.js
+++ src/db.js
@@ -2,7 +2,9 @@ import mongoose from 'mongoose'
 
   export const connectDB =async()=>{
   try{
- await mongoose.connect('mongodb://127.0.0.1:27017/AbogadosAPP')
+  //mongodb+srv://mepa1409:<password>@proyecto.xewlteu.mongodb.net/?retryWrites=true&w=majority&appName=Proyecto
+  await mongoose.connect('mongodb+srv://mepa1409:46365200@proyecto.xewlteu.mongodb.net/?retryWrites=true&w=majority&appName=Proyecto')
+ //await mongoose.connect('mongodb://127.0.0.1:27017/AbogadosAPP')
  console.log("BD esta conectada")
 
   } catch(error){
